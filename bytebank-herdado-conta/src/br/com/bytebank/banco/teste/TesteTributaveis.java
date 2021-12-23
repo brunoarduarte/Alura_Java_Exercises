38f84abaaabc656fa2818bdd39d89cfc8d649567
Bruno Duarte <brunoarduarte@gmail.com>
2021-12-23 20:30:41 -0300

    Criando packages e imports

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteTributaveis.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteTributaveis.java
@@ -1,8 +1,6 @@
-package teste;
+package br.com.bytebank.banco.teste;
 
-import modelo.CalculadorDeImposto;
-import modelo.ContaCorrente;
-import modelo.SeguroDeVida;
+import br.com.bytebank.banco.modelo.*;
 
 public class TesteTributaveis {
 
